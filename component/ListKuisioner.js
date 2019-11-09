bbc75eb953507f371fcb9e691320e54a194adf52
Adam Akbar <graha.asa@gmail.com>
2019-11-09 11:43:54 +0700

    Fix Checbox Handling

--- component/ListKuisioner.js
+++ component/ListKuisioner.js
@@ -4,37 +4,29 @@ import { BackHandler } from "react-native";
 import AsyncStorage from '@react-native-community/async-storage';
 
 
-function RenderSpesificSoal(jenis, pilihan) {
+function RenderSpesificSoal(jenis, pilihan, onAnswer) {
 
-    let [answer, setAnswer] = useState([]);
-    let [counter, setCounter] = useState(0);
-
-
-    const RenderButton = () => {
-        if (counter === props.data.length - 1) {
-            return (
-                <Button onPress={() => {
-                    alert("Submited");
-                }}>
-                    <Text>
-                        Submit
-                        </Text>
-                </Button>
-            )
-        }
-        else {
-            return (
-                <Button onPress={() => {
-                    setCounter(++counter);
-                }}>
-                    <Text>
-                        Next
-                        </Text>
-                </Button>
-            )
-        }
-    }
+    let [answer, setAnswer] = useState([
+        {
+            key: '0',
+            value: false
+        },
+        {
+            key: '1',
+            value: false
+        },
+        {
+            key: '2',
+            value: false
+        },
+        {
+            key: '3',
+            value: false
+        },
+    ]);
+    let [test, setTest] = useState(false);
     switch (jenis) {
+
         case 'pilihan_ganda':
             return (
                 <View>
@@ -43,7 +35,9 @@ function RenderSpesificSoal(jenis, pilihan) {
                             <Text>{pilihan.pilihanA}</Text>
                         </Left>
                         <Right>
-                            <Radio />
+                            <Radio onPress={() => {
+                                return onAnswer('A')
+                            }} />
                         </Right>
                     </ListItem>
                     <ListItem>
@@ -51,7 +45,9 @@ function RenderSpesificSoal(jenis, pilihan) {
                             <Text>{pilihan.pilihanB}</Text>
                         </Left>
                         <Right>
-                            <Radio />
+                            <Radio onPress={() => {
+                                return onAnswer('B')
+                            }} />
                         </Right>
                     </ListItem>
                     <ListItem>
@@ -59,7 +55,9 @@ function RenderSpesificSoal(jenis, pilihan) {
                             <Text>{pilihan.pilihanC}</Text>
                         </Left>
                         <Right>
-                            <Radio />
+                            <Radio onPress={() => {
+                                return onAnswer('C')
+                            }} />
                         </Right>
                     </ListItem>
                     <ListItem>
@@ -67,10 +65,11 @@ function RenderSpesificSoal(jenis, pilihan) {
                             <Text>{pilihan.pilihanD}</Text>
                         </Left>
                         <Right>
-                            <Radio />
+                            <Radio onPress={() => {
+                                return onAnswer('D')
+                            }} />
                         </Right>
                     </ListItem>
-                    <RenderButton />
                 </View>)
         case 'yesno':
             return (
@@ -80,7 +79,9 @@ function RenderSpesificSoal(jenis, pilihan) {
                             <Text>Yes</Text>
                         </Left>
                         <Right>
-                            <Radio />
+                            <Radio onPress={() => {
+                                return onAnswer('Yes')
+                            }} />
                         </Right>
                     </ListItem>
                     <ListItem>
@@ -88,10 +89,11 @@ function RenderSpesificSoal(jenis, pilihan) {
                             <Text>No</Text>
                         </Left>
                         <Right>
-                            <Radio />
+                            <Radio onPress={() => {
+                                return onAnswer('No')
+                            }} />
                         </Right>
                     </ListItem>
-                    <RenderButton />
 
                 </View>
             )
@@ -100,41 +102,64 @@ function RenderSpesificSoal(jenis, pilihan) {
                 <View>
                     <ListItem>
                         <Text>Answer </Text>
-                        <Input bordered onChange={(answerData) => {
-                            setAnswer(...answer, answerData);
+                        <Input bordered onChangeText={data => {
+                            return onAnswer(data);
                         }} />
                     </ListItem>
-                    <RenderButton />
 
                 </View>)
         case 'checkbox':
+
             return (
                 <View>
                     <ListItem>
-                        <CheckBox checked={true} />
+                        <CheckBox checked={answer[0].value} onPress={() => {
+                            let temp = answer;
+                            temp[0].value = !temp[0].value;
+                            setAnswer(temp);
+                            setTest(!test);
+                            return onAnswer(answer);
+                        }} />
                         <Body>
                             <Text>{pilihan.pilihanCB1}</Text>
                         </Body>
                     </ListItem>
                     <ListItem>
-                        <CheckBox checked={false} />
+                        <CheckBox checked={answer[1].value} onPress={() => {
+                            let temp = answer;
+                            temp[1].value = !temp[1].value;
+                            setAnswer(temp);
+                            setTest(!test);
+                            return onAnswer(answer);
+                        }} />
                         <Body>
                             <Text>{pilihan.pilihanCB2}</Text>
                         </Body>
                     </ListItem>
                     <ListItem>
-                        <CheckBox checked={false} />
+                        <CheckBox checked={answer[2].value} onPress={() => {
+                            let temp = answer;
+                            temp[2].value = !temp[2].value;
+                            setAnswer(temp);
+                            setTest(!test);
+                            return onAnswer(answer);
+                        }} />
                         <Body>
                             <Text>{pilihan.pilihanCB3}</Text>
                         </Body>
                     </ListItem>
                     <ListItem>
-                        <CheckBox checked={false} />
+                        <CheckBox checked={answer[3].value} onPress={() => {
+                            let temp = answer;
+                            temp[3].value = !temp[3].value;
+                            setAnswer(temp);
+                            setTest(!test);
+                            return onAnswer(answer);
+                        }} />
                         <Body>
                             <Text>{pilihan.pilihanCB4}</Text>
                         </Body>
                     </ListItem>
-                    <RenderButton />
 
                 </View>
             )
@@ -162,13 +187,43 @@ function RenderSoal(props) {
             return true;
         }
     }
+    const RenderButton = () => {
+        if (counter === props.data.length - 1) {
+            return (
+                <Button onPress={() => {
+                    alert("Submited");
+                }}>
+                    <Text>
+                        Submit
+                        </Text>
+                </Button>
+            )
+        }
+        else {
+            return (
+                <Button onPress={() => {
+                    setCounter(++counter);
+                }}>
+                    <Text>
+                        Next
+                        </Text>
+                </Button>
+            )
+        }
+    }
+
+    function onAnswer(data) {
+        console.log(data);
+    }
     return (
         <View>
             <Text>Pertanyaan ke {counter + 1}</Text>
             <Text>
                 {props.data[counter].pertanyaan_kuisioner}
             </Text>
-            {RenderSpesificSoal(props.data[counter].jenis_pertanyaan, props.data[counter])}
+            {RenderSpesificSoal(props.data[counter].jenis_pertanyaan, props.data[counter], onAnswer)}
+            <RenderButton />
+
         </View>
     )
 
@@ -177,12 +232,14 @@ function RenderSoal(props) {
 
 export default function ListKuisioner(props) {
     let [soal, setSoal] = useState();
+    let [counter, setCounter] = useState(0);
 
     async function getData() {
         let data = await AsyncStorage.getItem(props.type);
         let parse = JSON.parse(data);
         setSoal(parse);
     }
+
 
 
     useEffect(() => {
